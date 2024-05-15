90567cf3c5c061c5be5d94b2abfe931d559d9ee8
Jazael Perez <131943183+perezjazaelr@users.noreply.github.com>
2024-05-15 18:59:19 +0800

    Add files via upload

---
+++ scriot.js
@@ -0,0 +1,150 @@
+/* MAIN */
+function home(){
+    location.href=("landingpage.html");
+}
+
+function subtopic(){
+    location.href=("sb-introduction.html");
+}
+
+
+/* LANDING PAGE */
+function introduction(){
+    location.href=("sb-introduction.html");
+}
+function operator(){
+    location.href=("sb-operators.html");
+}
+function crtlState(){
+    location.href=("sb-CrtlState.html");
+}
+
+
+/* INTRODUCTION*/
+function hello(){
+    location.href=("intro-hello.html");
+}
+function echo(){
+    location.href=("intro-echo.html");
+}
+function echo(){
+    location.href=("intro-echo.html");
+}
+function constant(){
+    location.href=("intro-constant.html");
+}
+function typecasting(){
+    location.href=("intro-typeCasting.html");
+}
+function boolean(){
+    location.href=("intro-boolean.html");
+}
+function math(){
+    location.href=("intro-math.html");
+}
+function file(){
+    location.href=("intro-file.html");
+}
+function comment(){
+    location.href=("intro-comment.html");
+}
+function vardump(){
+    location.href=("intro-vardump.html");
+}
+function magicconstant(){
+    location.href=("intro-magicconstant.html");
+}
+function typejuggling(){
+    location.href=("intro-typejug.html");
+}
+function integer(){
+    location.href=("intro-integer.html");
+}
+function heredoc(){
+    location.href=("intro-heredoc.html");
+}
+function date(){
+    location.href=("intro-date.html");
+}
+function variable(){
+    location.href=("intro-variables.html");
+}
+function $and(){
+    location.href=("intro-$and&&.html");
+}
+function datatype(){
+    location.href=("intro-datatype.html");
+}
+function string(){
+    location.href=("intro-string.html");
+}
+function io(){
+    location.href=("intro-io.html");
+}
+function compound(){
+    location.href=("intro-compound.html");
+}
+function scalar(){
+    location.href=("intro-scalar.html");
+}
+
+/* OPERATORS */
+function Arithmatic(){
+    location.href=("oper-Arithmatic.html");
+}
+function Comparison(){
+    location.href=("oper-Comparison.html");
+}
+function Logical(){
+    location.href=("oper-Logical.html");
+}
+function Assignment(){
+    location.href=("oper-Assignment.html");
+}
+function Stringoper(){
+    location.href=("oper-Stringoper.html");
+}
+function Arrayoper(){
+    location.href=("oper-Arrayoper.html");
+}
+function Conditional(){
+    location.href=("oper-Conditional.html");
+}
+function Spread(){
+    location.href=("oper-Spread.html");
+}
+function Null(){
+    location.href=("oper-Null.html");
+}
+function Spaceship(){
+    location.href=("oper-Spaceship.html");
+}
+
+/* CONTROL STATEMENT */
+function DecisionMaking(){
+    location.href=("control-DecisionMaking.html");
+}
+function WhileLoop(){
+    location.href=("control-WhileLoop.html");
+}
+function IfElse(){
+    location.href=("control-IfElse.html");
+}
+function ForLoop(){
+    location.href=("control-ForLoop.html");
+}
+function DoWhile(){
+    location.href=("control-DoWhile.html");
+}
+function ContinueState(){
+    location.href=("control-ContinueState.html");
+}
+function SwitchState(){
+    location.href=("control-SwitchState.html");
+}
+function ForeachLoop(){
+    location.href=("control-ForeachLoop.html");
+}
+function BreakState(){
+    location.href=("control-BreakState.html");
+}
